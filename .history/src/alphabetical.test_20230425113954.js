5227f8e448a01d26f40fb3fdec4e6b023ebd09fc
Lehuyhuynh <huynhle15122000@gmail.com>
2023-04-25 11:50:48 +0700

    Write a program that takes a list of strings as input and sorts the list in alphabetical order.

---
+++ .history/src/alphabetical.test_20230425113954.js
@@ -0,0 +1,25 @@
+const shorteststringr  = require("./shorteststring");
+
+test("shortest string in list('hello','world') is ('hello','world') of length 5 ", () => {
+  expect(shorteststringr("hello","world")).toBe(("hello","world"));
+});
+
+test('Sorts an array of positive numbers in ascending order', () => {
+  const arr = [5, 2, 8, 1, 6];
+  const sortedArr = sort(arr);
+  expect(sortedArr).toEqual([1, 2, 5, 6, 8]);
+});
+
+test("shortest string in list(367,'js') is (js) of length 2", () => {
+  expect(shorteststringr(367,'js')).toBe('js');
+});
+
+test("shortest string in list('javascipt','js') is (js) of length 2", () => {
+  expect(shorteststringr('javascript','js')).toBe('js');
+});
+
+test("shortest string in list('html','css') is ('css') of length 3 ", () => {
+  expect(shorteststringr("html","css")).toBe("css");
+});
+
+
---
+++ .history/src/alphabetical_20230425113630.js
@@ -0,0 +1,2 @@
+function alphabetical
+module.exports=alphabetical
